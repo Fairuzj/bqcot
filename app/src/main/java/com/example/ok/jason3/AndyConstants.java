7045fe214db4292d3a9e6dced7ef83b674b59ccf
fairuzj <fairuzjmf@gmail.com>
2019-02-25 15:09:30 +0700

    eaa

---
+++ app/src/main/java/com/example/ok/jason3/AndyConstants.java
@@ -0,0 +1,19 @@
+package com.example.ok.jason3;
+
+public class AndyConstants {
+
+    public class ServiceCode {
+        public static final int HOME = 1;
+    }
+    // web service url constants
+    public class ServiceType {
+        public static final String URL = "https://demonuts.com/Demonuts/JsonTest/Tennis/json_parsing.php";
+    }
+    // webservice key constants
+    public class Params {
+        public static final String ID = "id";
+        public static final String NAME = "name";
+        public static final String COUNTRY = "country";
+        public static final String CITY = "city";
+    }
+}
